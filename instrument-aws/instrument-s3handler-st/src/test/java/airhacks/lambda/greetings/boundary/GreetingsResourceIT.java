cab8edcdfd611cb3df5a752e6b151ca31b6a5b5b
Jim O'Mulloy <jimomulloy@gmail.com>
2023-04-28 17:50:48 +0100

    st

---
+++ instrument-aws/instrument-s3handler-st/src/test/java/airhacks/lambda/greetings/boundary/GreetingsResourceIT.java
@@ -0,0 +1,28 @@
+package airhacks.lambda.greetings.boundary;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+//@QuarkusTest
+public class GreetingsResourceIT {
+
+	@Inject
+	@RestClient
+	GreetingsResource resource;
+
+	@Inject
+	@ConfigProperty(name = "base_uri/mp-rest/url")
+	String baseURI;
+
+	// @Test
+	public void hello() {
+		var message = this.resource.content();
+		assertNotNull(message);
+		System.out.println(message);
+	}
+
+}
