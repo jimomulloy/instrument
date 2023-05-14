c3a0df788220d25b5640f57c79946da3d20bdc6c
jimomulloy <jimomulloy@gmail.com>
2023-05-14 13:52:57 +0100

    sts

---
+++ instrument-aws/instrument-s3handler/src/main/java/jomu/instrument/aws/s3handler/Greetings.java
@@ -0,0 +1,24 @@
+package jomu.instrument.aws.s3handler;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class Greetings {
+    
+    static String message = System.getenv("message");
+
+    public Greetings() {
+        System.out.println("initialized with configuration: " + message);
+    }
+
+    public String onEvent(Map<String, String> event) {
+        System.out.println("received: " + event);
+        return event
+        .entrySet()
+        .stream()
+        .map(e -> e.getKey() + "->" + e.getValue())
+        .collect(Collectors.joining(","));
+    }
+    
+}
