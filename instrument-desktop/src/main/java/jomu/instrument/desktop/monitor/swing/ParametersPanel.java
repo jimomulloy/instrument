c64285d83b77f1bced85ff6a986a84c4824ae4e9
Jim O'Mulloy <jimomulloy@gmail.com>
2024-03-09 20:24:42 +0000

    ai

--- instrument-desktop/src/main/java/jomu/instrument/desktop/monitor/swing/ParametersPanel.java
+++ instrument-desktop/src/main/java/jomu/instrument/desktop/monitor/swing/ParametersPanel.java
@@ -53,7 +53,8 @@ public class ParametersPanel extends JPanel {
 
 	private final static Integer[] fftSizes = { 256, 512, 1024, 2048, 4096, 8192, 16384, 22050, 32768, 65536, 131072 };
 	private final static String[] styles = { "default", "normalised", "ensemble", "guitar", "piano", "vocal",
-			"vocal-folk", "vocal-male", "vocal-female", "tapes", "robin", "birds", "blackbird", "bird-alt1",
+			"vocal-folk", "vocal-male", "vocal-female", "tapes", "tapes-fast", "robin", "birds", "blackbird",
+			"bird-alt1",
 			"bird-best", "bird-scaled", "bird-mix", "classical", "folk", "folky", "folky-clean", "folk-pluck", "bach",
 			"birds-bach", "beethoven", "brass", "compresschord", "piano-harp", "epiano", "epiano-arp", "epiano-chords",
 			"epiano-chords-staccato", "guitarstrum", "eguitar", "hpschord", "neon", "neon-peaked", "neon-synth",
