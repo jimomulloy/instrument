9a9261cd9eab9e47373cdc7766b46c63d8e5e34e
Jim O'Mulloy <jimomulloy@gmail.com>
2025-09-13 13:23:12 +0100

    gtt

--- instrument-desktop/src/main/java/jomu/instrument/desktop/monitor/swing/ParametersPanel.java
+++ instrument-desktop/src/main/java/jomu/instrument/desktop/monitor/swing/ParametersPanel.java
@@ -53,7 +53,9 @@ public class ParametersPanel extends JPanel {
 
 	private final static Integer[] fftSizes = { 256, 512, 1024, 2048, 4096, 8192, 16384, 22050, 32768, 65536, 131072 };
 	private final static String[] styles = {
-			"default", "chords", "melody", "melody-clean", "melody-finzi", "chords-voice",
+			"default",
+			"chords", "chords-blocks", "chords-voice", "chords-lines", "chords-voice",
+			"melody", "melody-clean", "melody-finzi",
 			"genesis-white", "envwhiten", "gencqsquare", "fnzy", "watcher", "beet", "dawson", "yes",
 			"sustain", "metronome",
 			"vw-hps-peaks",
